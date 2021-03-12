df1b12f9bf19f6a48a65a6cd35bd9b84ef41b4db
Lakshya-Coder <lakshyagupta7089@gmail.com>
2021-03-12 15:10:54 +0530

    Commit

--- GamePanel.java
+++ GamePanel.java
@@ -1,9 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
 import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
